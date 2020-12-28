c32f8b157339165686038c664a69c6e79f08b9b7
rusa6111 <kaikai.asdfghjkl@gmail.com>
2020-12-28 23:32:31 +0900

    add _blank to a-tag

--- OMCproblems/problemsloader.js
+++ OMCproblems/problemsloader.js
@@ -13,7 +13,7 @@ $(function(){
     var c_dat = JSON.parse(xmlhttp.responseText);
     for(var i=0; i<7; i++){
       if(i == 0){
-        r += "<td><a class='" + contestGroup + "-contest' href='https://onlinemathcontest.com/contests/" + c_name + "/'>" + c_name + "</a></td>";
+        r += "<td><a target='_blank' class='" + contestGroup + "-contest' href='https://onlinemathcontest.com/contests/" + c_name + "/'>" + c_name + "</a></td>";
       }else{
         var alpha = "ABCDEF"[i-1];
         var p_dat = c_dat[i-1];
@@ -31,7 +31,7 @@ $(function(){
         }
         var diff_fill = diff >= 2800 ? 100 : (diff < 0 ? 0 :(diff - Math.floor(diff/400)*400) / 4);
         r +="<td><span class='show-diff' data-tooltip='Difficulty:" + Math.floor(diff) + "'><div class='" + p_class + "-circle' style='background: linear-gradient(to top," + p_color + " 0%, " + p_color + " " + diff_fill + "%, #0000 " + diff_fill + "%, #0000 100%);'></div>";
-        r += "<a class = '" + p_class + "-diff' href='https://onlinemathcontest.com/contests/" + c_name + "/tasks/" + c_name + "_" + alpha + "'>" + c_name + "-" + alpha + "</a></span></td>";
+        r += "<a target='_blank' class = '" + p_class + "-diff' href='https://onlinemathcontest.com/contests/" + c_name + "/tasks/" + c_name + "_" + alpha + "'>" + c_name + "-" + alpha + "</a></span></td>";
       }
     }
     $("#problems-body").append("<tr>" + r + "</tr>");
