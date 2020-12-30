bb677ee3b8e4e6253bf1e2a5b0bf4f0998fe2865
rusa6111 <kaikai.asdfghjkl@gmail.com>
2020-12-30 15:43:59 +0900

    make FAQ

--- OMCproblems/problemsloader.js
+++ OMCproblems/problemsloader.js
@@ -13,6 +13,31 @@ $(function(){
   var sigmoid = function(a, d, r){
     return 1/(1 + Math.exp(-a * (r - d)));
   }
+
+
+  var makeDiffCircleHP = function(d, r){
+    if(d > 2800){
+      p_color = "#F00";
+    }else{
+      p_color = ["#888", "#840", "#080", "#0CC", "#00F", "#CC0", "#F80"][Math.max(0, Math.floor(d / 400))];
+    }
+    var diff_fill = d >= 2800 ? 100 : (d < 0 ? 0 :(d - Math.floor(d/400)*400) / 4);
+    var style = ""
+    style += "border:solid 1px " + p_color + ";";
+    style += "height: " + r + ";width: " + r + ";";
+    style += "background: linear-gradient(to top," + p_color + " 0%, " + p_color + " " + diff_fill + "%, #0000 " + diff_fill + "%, #0000 100%);";
+    return "<div class='diff-circle' style='" + style + "'></div>";
+  }
+
+  var diffColor = function(d){
+    if(d >= 2800){
+      r = "red";
+    }else{
+      r = ["gray", "brown", "green", "cyan", "blue", "yellow", "orange"][Math.max(0, Math.floor(d / 400))];
+    }
+    return r
+  }
+
   var makePH = function(){
     problemsHTML = {}
     for(var ci=0; ci<Number(contests[0]); ci++){
@@ -36,22 +61,19 @@ $(function(){
           var p_class = "none";
           var p_color = "#000#"
           if(p_dat["rated"]){
-            if(diff >= 2800){
-              p_class = "red";
-              p_color = "#F00";
-            }else{
-              p_class = ["gray", "brown", "green", "cyan", "blue", "yellow", "orange"][Math.max(0, Math.floor(diff / 400))];
-              p_color = ["#888", "#840", "#080", "#0CC", "#00F", "#CC0", "#F80"][Math.max(0, Math.floor(diff / 400))];
-            }
+            p_class = diffColor(diff);
           }
           var diff_fill = diff >= 2800 ? 100 : (diff < 0 ? 0 :(diff - Math.floor(diff/400)*400) / 4);
-          r = "<span class='show-diff' data-tooltip='Difficulty:" + Math.floor(diff) + sp + "'><div class='" + p_class + "-circle' style='background: linear-gradient(to top," + p_color + " 0%, " + p_color + " " + diff_fill + "%, #0000 " + diff_fill + "%, #0000 100%);'></div>";
+          r = "<span class='show-diff' data-tooltip='Difficulty:" + Math.floor(diff) + sp + "'>"
+          if(p_dat["rated"]) r += makeDiffCircleHP(diff, 12);
           r += "<a target='_blank' class = '" + p_class + "-diff' href='https://onlinemathcontest.com/contests/" + c_name + "/tasks/" + c_name + "_" + alpha + "'>" + c_name + "-" + alpha + "</a></span>";
         }
         problemsHTML[c_name][i] = r;
       }
     }
   }
+
+
   var show = function(){
     var tb = document.getElementById("problems-body");
     tb.innerHTML = "";
@@ -64,6 +86,8 @@ $(function(){
       tb.innerHTML += "<tr>" + h + "</tr>";
     }
   }
+
+
   var getRating = function(){
     xmlhttp.open("GET", "rating.txt", false);
     xmlhttp.send(null);
@@ -73,17 +97,24 @@ $(function(){
       rating_list[p[0]] = Number(p[1]);
     }
   }
+
+
   document.getElementById("userid_ok").onclick = function(e){
     var value = document.getElementById("userid").value;
+    var ratingHTML = document.getElementById("rating");
     if(value in rating_list){
       rating = rating_list[value];
+      ratingHTML.innerHTML = "Your rating: " + makeDiffCircleHP(rating, 12) + "<span class='" + diffColor(rating) + "-diff'>" + rating + "</span>";
     }else{
       rating = null;
+      ratingHTML.innerHTML = "";
     }
     makePH();
     show();
   }
-
+  window.onkeydown = function(e){
+    if(e.key == "Enter") document.getElementById("userid_ok").onclick();
+  }
 
   makePH();
   show();
